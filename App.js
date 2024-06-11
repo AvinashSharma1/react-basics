07315eeb063471c11ccac54209d39f5b24af42af
Avinash Sharma <avinash.sharma@leadventure.com>
2024-06-11 22:24:40 +0530

    Adding H1 tag attribute id and class

--- App.js
+++ App.js
@@ -1,3 +1,3 @@
-const heading = React.createElement("h1",{},"This is H1 heading from React App.js !!");
+const heading = React.createElement("h1",{id: "heading", class:"heading", data_testAttr:"Test Attribute"},"This is H1 heading from React App.js !!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
