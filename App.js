d38f35c1ae449bfdb93f128ee5131df4d8f823b6
Avinash Sharma <avinash.sharma@leadventure.com>
2024-06-11 22:53:27 +0530

    Adding Nested child

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
-const heading = React.createElement("h1",{id: "heading", class:"heading", data_testAttr:"Test Attribute"},"This is H1 heading from React App.js !!");
+/**
+ * <div>
+ *      <div>
+ *              <h1>This is H1 child!</h1>
+ *      <div>
+ * </div>
+ * 
+ * 
+ */
+
+const heading = React.createElement("h1",{id: "heading", className:"heading", testAttr:"Test Attribute"},"This is H1 heading from Nested child !!");
+const children = React.createElement("div",{id:"child"}, heading);
+const parent = React.createElement("div",{id:"parent"}, children);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
